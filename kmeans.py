af0d6ebd6cdf5c4279f46b6a4d375ecfcb75e054
Odilia Lirani <oclirani@gmail.com>
2016-11-29 17:07:24 -0500

    initial commit

---
+++ kmeans.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+'''
+Purdue CS 390 - Fall 2016
+Odilia Lirani
+0027603470
+HW 4
+Prof. Dan Goldwasser
+'''
+import sys
+
+print "# args ", len(sys.argv)
+arguments = sys.argv
+if (len(arguments) < 4):
+	print "Invalid arguments"
+else:
+	print "cont..."
