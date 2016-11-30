b0a31809342605cf03db32be970d7298e1c991ac
Odilia Lirani <oclirani@gmail.com>
2016-11-30 14:59:44 -0500

    data extracted

--- kmeans.py
+++ kmeans.py
@@ -7,11 +7,57 @@ Odilia Lirani
 HW 4
 Prof. Dan Goldwasser
 '''
-import sys
 
-print "# args ", len(sys.argv)
+import sys
+import csv
+
+# Need to store latitude, longitude, revCount, checkins to calc centroid
+# 7 (lat) 8 (long) 10 (revCount) 11 (checkins)
+# to find the centroids, we need to calculate the 
+
 arguments = sys.argv
-if (len(arguments) < 4):
+if (len(arguments) < 3):
 	print "Invalid arguments"
 else:
-	print "cont..."
+	dataFile = arguments[1]
+	k = arguments[2]
+	counter = 0
+	data = []
+	with open(dataFile, 'rU') as file:
+		reader = csv.reader(file, dialect=csv.excel_tab)
+		for line in reader:
+			if (counter == 0):
+				counter += 1
+				continue
+			for a in line[0].split(','):
+				data.append(a);
+
+			if (counter > 1):
+				counter = 1
+				# DO SOMETHING HERE
+				
+			counter += 1
+		print data[7]
+		print data[8]
+		print data[10]
+		print data[11]
+	'''with open(dataFile, 'rb') as file:
+		r = csv.reader(open(self.file, 'rU'), dialect=csv.excel_tab)
+		for row in r:
+			print row
+	for num,line in enumerate(lines, 1):
+			print line.split()
+			print '\n'
+			break
+	tabHeader = next(reader)
+		ite = True
+		while (ite == True):
+			for i in range(0, 2):
+				line = next(reader)
+				for num, l in enumerate(line, 1):
+					print l
+				if (line == ''):
+					ite == False
+					break
+				#print line
+	'''
