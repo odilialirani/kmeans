1377068120b6d742af227fda512f0157e8cd8ab7
Odilia Lirani <oclirani@gmail.com>
2016-11-30 22:12:16 -0500

    data extracted, init centroinds picked

--- kmeans.py
+++ kmeans.py
@@ -8,8 +8,8 @@ HW 4
 Prof. Dan Goldwasser
 '''
 
-import sys
-import csv
+import sys, csv, random
+import numpy as np
 
 # Need to store latitude, longitude, revCount, checkins to calc centroid
 # 7 (lat) 8 (long) 10 (revCount) 11 (checkins)
@@ -22,9 +22,10 @@ else:
 	dataFile = arguments[1]
 	k = arguments[2]
 	counter = 0
+	tabRow = []
+	#data = np.array()
 	data = []
-	o = 0
-	skip = True;
+	centroids = []
 	with open(dataFile, 'rU') as file:
 		reader = csv.reader(file, dialect=csv.excel_tab)
 		for line in reader:
@@ -41,36 +42,20 @@ else:
 				print data[7], ',', data[8], ',', data[10], ',', data[11]
 				data = []'''
 			if (counter == 0):
+				# this will eliminate the column names row
 				counter += 1
 				continue
 			for a in line[0].split(','):
-				data.append(a);
-
-			if (counter > 1):
-				counter = 1
-				# DO SOMETHING HERE
-				print data[7], ',', data[8], ',', data[10], ',', data[11]
-				data[:] = []
-				continue
-			counter += 1
+				tabRow.append(a);
+			#print tabRow[3], ',', tabRow[4], ',', tabRow[6], ',', tabRow[7]
+			data.append([tabRow[3], tabRow[4], tabRow[6], tabRow[7]])
+			tabRow[:] = []
+		# Select k random points
+		for i in range(int(k)):
+			index = random.randint(0, len(data)-1)
+			centroids.append(data[index])
 		
-	'''with open(dataFile, 'rb') as file:
-		r = csv.reader(open(self.file, 'rU'), dialect=csv.excel_tab)
-		for row in r:
-			print row
-	for num,line in enumerate(lines, 1):
-			print line.split()
-			print '\n'
-			break
-	tabHeader = next(reader)
-		ite = True
-		while (ite == True):
-			for i in range(0, 2):
-				line = next(reader)
-				for num, l in enumerate(line, 1):
-					print l
-				if (line == ''):
-					ite == False
-					break
-				#print line
-	'''
+		
+
+
+
